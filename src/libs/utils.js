0e8fe975acfdf1c6b0f17855013e6c9a09e3278e
Kelsi <thacker.kl.01@gmail.com>
2023-07-12 17:18:07 -0700

    fixed more issues

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -1,10 +1,3 @@
-// let md5 = require("md5");
-import MD5 from "crypto-js/md5";
-
-const getHash = (ts, privateKey, publicKey) => {
-  return MD5(ts + privateKey + publicKey).toString();
-};
-
 let API_URL = process.env.REACT_APP_BASE_URL;
 
 const fetchHeros = async (name) => {
@@ -13,8 +6,7 @@ const fetchHeros = async (name) => {
   let ts = Date.now().toString();
   let apiKey = process.env.REACT_APP_API_KEY;
   let privateKey = process.env.REACT_APP_PRIVATE_KEY;
-  let hash = getHash(ts, privateKey, apiKey);
-  let url = `${heroUrl}?ts=${ts}&apikey=${apiKey}&hash=${hash}&nameStartsWith=${name}`;
+  let url = `${heroUrl}?ts=${ts}&apikey=${apiKey}&nameStartsWith=${name}`;
 
   try {
     let response = await fetch(url);
@@ -33,8 +25,7 @@ const fetchHero = async (id) => {
   let ts = Date.now().toString();
   let apiKey = process.env.REACT_APP_API_KEY;
   let privateKey = process.env.REACT_APP_PRIVATE_KEY;
-  let hash = getHash(ts, privateKey, apiKey);
-  let url = `${heroUrl}?ts=${ts}&apikey=${apiKey}&hash=${hash}`;
+  let url = `${heroUrl}?ts=${ts}&apikey=${apiKey}`;
 
   try {
     let response = await fetch(url);
